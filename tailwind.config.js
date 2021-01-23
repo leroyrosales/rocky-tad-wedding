68481de1e4a0bb26a9d991bbb7d304de738557b3
Leroy Rosales <leroyrosales@gmail.com>
2021-01-23 13:15:56 -0600

    Adds in animation library

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,21 @@ module.exports = {
     extend: {
       colors: {
         wine: "#470000"
-      }
+      },
+      animation: {
+        fadeIn: "fadeIn 2s ease-in forwards"
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      },
     },
   },
   variants: {
     extend: {},
+    animation: ["motion-safe"],
   },
   plugins: [],
 }
